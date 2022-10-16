4c29d8134ce9043e1b45460fcdfa96d4a2d552ed
day0ung <jdy9436@naver.com>
2022-10-16 22:48:51 +0900

    Lotto Dto column name 매핑 추가 (name을 변수명그대로 읽어서 추가)

--- back-end/lotto/src/main/java/mirea/on/lotto/entity/Lotto.java
+++ back-end/lotto/src/main/java/mirea/on/lotto/entity/Lotto.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -17,25 +18,64 @@ public class Lotto {
 
     @Id // 기본키가 될 변수를 의미
     private Long id;
-
+    @Column(name="RAFFLE_ROUND")
     private Long raffleRound;
+
+    @Column(name="RAFFLE_DATE")
     private Date raffleDate;
+
+    @Column(name="FIRST_WIN_COUNT")
     private Long firstWinCount;
-    private Long firstWinAmout;
+
+    @Column(name="FIRST_WIN_AMOUNT")
+    private Long firstWinAmount;
+
+    @Column(name="SECOND_WIN_COUNT")
     private Long secondWinCount;
-    private Long secondWinAmout;
+
+    @Column(name="SECOND_WIN_AMOUNT")
+    private Long secondWinAmount;
+
+    @Column(name="THIRD_WIN_COUNT")
     private Long thirdWinCount;
-    private Long thirdWinAmout;
+
+    @Column(name="THIRD_WIN_AMOUNT")
+    private Long thirdWinAmount;
+
+    @Column(name="FOURTH_WIN_COUNT")
     private Long fourthWinCount;
-    private Long fourthWinAmout;
+
+    @Column(name="FOURTH_WIN_AMOUNT")
+    private Long fourthWinAmount;
+
+    @Column(name="FIFTH_WIN_COUNT")
     private Long fifthWinCount;
-    private Long fifthWinAmout;
+
+    @Column(name="FIFTH_WIN_AMOUNT")
+    private Long fifthWinAmount;
+
+    @Column(name="BALL_ONE")
     private Long ballOne;
+
+    @Column(name="BALL_TWO")
     private Long ballTwo;
+
+    @Column(name="BALL_THREE")
     private Long ballThree;
+
+    @Column(name="BALL_FOUR")
     private Long ballFour;
-    private Long ballFive ;
+
+    @Column(name="BALL_FIVE")
+    private Long ballFive;
+
+    @Column(name="BALL_SIX")
     private Long ballSix;
+
+    @Column(name="BALL_BONUS")
     private Long ballBonus;
+
+    @Column(name="REG_DT")
+    private Date regDt;
 
 }
