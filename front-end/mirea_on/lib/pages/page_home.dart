074f26700160f06d33f99eb3c1d271926f743064
day0ung <jdy9436@naver.com>
2023-06-15 16:27:34 +0900

    feat : shared_preference 추가 (key-value 형태의 데이터를 디스크에 저장)

--- front-end/mirea_on/lib/pages/page_home.dart
+++ front-end/mirea_on/lib/pages/page_home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mirea_on/utils/colors.dart';
-import '../models/numbers.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../utils/decoration.dart';
+import '../widgets/lottery/basic_raffle.dart';
 
 
 class HomePage extends StatefulWidget{
@@ -11,72 +12,106 @@ class HomePage extends StatefulWidget{
   State<StatefulWidget>  createState() => _HomePage();
 }
 
+
 class _HomePage extends State<HomePage> {
   List<List<int>> grids = [];
   List<int> selectedNumbers = [];
 
+  late SharedPreferences _prefs;
+
+  @override
+  void initState() {
+    super.initState();
+    loadGrids();
+  }
+
+  Future<void> loadGrids() async {
+    _prefs = await SharedPreferences.getInstance();
+    final savedGrids = _prefs.getStringList('grids');
+    if (savedGrids != null) {
+      setState(() {
+        grids = savedGrids
+            .map((grid) => grid.split(',').map(int.parse).toList())
+            .toList();
+      });
+    } else {
+      setState(() {
+        grids = []; // 그리드 리스트 초기화
+      });
+    }
+  }
+
+  Future<void> saveGrids() async {
+    final gridStrings = grids.map((grid) => grid.join(',')).toList();
+    await _prefs.setStringList('grids', gridStrings);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          title: const Center(
-            child: Icon(
-              Icons.terrain,
-              color: ColorUtils.mainColor,
-              size: 40,
-            ),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Center(
+          child: Icon(
+            Icons.terrain,
+            color: ColorUtils.mainColor,
+            size: 40,
           ),
-          actions: const [
-            SizedBox(width: 30, height: 30),
-          ],
-          iconTheme: const IconThemeData(color: ColorUtils.mainColor),
         ),
-
-        body: Container(
-          padding: const EdgeInsets.all(5),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                decoration: CardDecoration(),
-                height: 160,
-                child: Row(
-                  children: [
-                    Text('첫째줄 텍스트'),
-                  ],
-                ),
+        actions: const [
+          SizedBox(width: 30, height: 30),
+        ],
+        iconTheme: const IconThemeData(color: ColorUtils.mainColor),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: CardDecoration(),
+              height: 160,
+              child: Row(
+                children: [
+                  Text('첫째줄 텍스트'),
+                ],
               ),
-              Expanded(
-                child: Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  child: GridView.count(
-                    childAspectRatio: 0.7, // 너비:높이 비율 조절
-                    mainAxisSpacing: 15, // 아이템 간의 수직 간격
-                    crossAxisSpacing: 12, // 아이템 간의 수평 간격
+            ),
+            Expanded(
+              child: Container(
+                margin: const EdgeInsets.only(top: 20),
+                child: GridView.builder(
+                  itemCount: grids.length + 1, // 그리드와 버튼 개수
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    children: [
-                      for (var grid in grids) // 변경: grids 리스트를 반복하여 그리드 생성
-                        CardItem(text: grid.toString()),
-                      CardButton(
+                    mainAxisSpacing: 15,
+                    crossAxisSpacing: 12,
+                    childAspectRatio: 0.7,
+                  ),
+                  itemBuilder: (context, index) {
+                    if (index < grids.length) {
+                      return CardItem(text: grids[index].toString());
+                    } else {
+                      return CardButton(
                         onNumbersSelected: (numbers) {
                           setState(() {
                             selectedNumbers = numbers;
-                            grids.add(numbers); // 변경: grids에 선택한 숫자 그리드 추가
+                            grids.add(numbers); // 선택한 그리드를 추가
+                            saveGrids(); // 그리드 저장
                           });
                         },
-                      ),
-                    ],
-                  ),
+                      );
+                    }
+                  },
                 ),
               ),
-            ],
-          ),
-        )
+            ),
+          ],
+        ),
+      ),
     );
-
   }
 }
 
@@ -96,7 +131,6 @@ class CardItem extends StatelessWidget {
   }
 }
 
-
 class CardButton extends StatelessWidget {
   final void Function(List<int>)? onNumbersSelected;
 
@@ -111,7 +145,7 @@ class CardButton extends StatelessWidget {
         onTap: () async {
           final selectedNumbers = await Navigator.push<List<int>>(
             context,
-            MaterialPageRoute(builder: (context) => NumberPage()),
+            MaterialPageRoute(builder: (context) => BasicWidget()),
           );
 
           if (selectedNumbers != null && onNumbersSelected != null) {
